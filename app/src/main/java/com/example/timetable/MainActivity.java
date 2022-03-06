f6457510fb4e2a00f007a393b3ba20210f3784b9
waqas <waqaswaseem679@gmail.com>
2022-03-06 14:21:57 +0500

    time to check commit

---
+++ app/src/main/java/com/example/timetable/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.timetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //commit time is at 2.22pm
+    }
+}
